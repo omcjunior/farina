ad54b768a909e84d4cb2c7eef4c8a1f28b20fcb1
Osvaldo Moreira <osvaldomoreira@gmail.com>
2018-09-19 17:13:43 -0400

    Cabecalho do layout do site

---
+++ resources/views/layouts/site.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+
+<html lang="pt-br">
+
+<head>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <meta name="keywords" content="ensino, aprovação, aprovados, aprovações, aprova, vestibular, medicina, direito, engenharia, escola">
+    <meta name="description" content="Desenvolver e proliferar saberes com o objetivo de contribuir com a formação de pessoas.">
+    <meta name="author" content="Osvaldo Moreira">
+    <meta name="robots" content="index,follow">
+
+    <link rel="shortcut icon" href="./favicon.ico">
+    <link rel="apple-touch-icon image_src" href="./apple-touch-icon.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header id="header--main">
+            <div id="header--wrap--menu" class="crop width--calc">
+                <div id="box--logomarca">
+                    <a href="#">
+                        <img class="logo--header" src="imgs/structor/logomarca.png" alt="Farina Sistema de Ensino">
+                    </a>
+                </div>
+                <div id="box--main--menu">
+                    <ul class="main--menu">
+                        <li><a href="#" alt="Página Inicial">Home</a></li>
+                        <li><a href="#" alt="Sobre Nós">Sobre Nós</a></li>
+                        <li><a href="#" alt="Depoimentos">Depoimentos</a></li>
+                        <li><a href="#" alt="Fotos">Fotos</a></li>
+                        <li><a href="#" alt="Blog">Blog</a></li>
+                        <li><a href="#" alt="Portifólio">Portifólio</a></li>
+                        <li><a href="#" alt="Contato">Contato</a></li>
+                    </ul>
+                </div>
+                <!-- <div id="box--social">
+                    <ul>
+                        <li><a href="#"><span class="fab fa-facebook-square fa-1x"></span></a></li>
+                        <li><a href="#"><span class="fab fa-instagram fa-1x"></span></a></li>
+                        <li><a href="#"><span class="fab fa-twitter-square fa-1x"></span></a></li>
+                        <li><a href="3"><span class="fas fa-globe-americas fa-1x"></span></a></li>
+                    </ul>
+                </div> -->
+                <div class="button--menu">
+                    <i class="fas fa-bars fa-2x color-white"></i>
+                    <!-- <i class="fas fa-times fa-2x color-white"></i> -->
+                </div>
+            </div>
+        </header>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+        <footer id="footer--main">
+            
+            <span class="fone">(65) 3628-3033<span>
+            <span class="fone">(65) 99958-7617<span>
+            <address>Miguel Sutil, 1695 – Jardim Guanabara, Cuiabá – MT, 78010-500 matricula@escoladofarina.com.br</address>
+            <div id="box--social">
+                <ul>
+                    <li><a href="#"><span class="fab fa-facebook-square fa-1x"></span></a></li>
+                    <li><a href="#"><span class="fab fa-instagram fa-1x"></span></a></li>
+                    <li><a href="#"><span class="fab fa-twitter-square fa-1x"></span></a></li>
+                    <li><a href="3"><span class="fas fa-globe-americas fa-1x"></span></a></li>
+                </ul>
+            </div>
+
+            IMG
+            <p>Copyright - Todos os Direitos a Farias Sistema de Ensino</p>
+        </footer>
+    </div>
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,68 +1,27 @@
-<!doctype html>
+@extends('layouts.site')
 
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
 
-        <meta name="keywords" content="ensino, aprovação, aprovados, aprovações, aprova, vestibular, medicina, direito, engenharia, escola">
-        <meta name="description" content="Desenvolver e proliferar saberes com o objetivo de contribuir com a formação de pessoas.">
-        <meta name="author" content="Osvaldo Moreira">
-        <meta name="robots" content="index,follow">
 
-        <title>Farina Sistema de Ensino</title>
-        
-        <link rel="shortcut icon" href="./favicon.ico">
-        <link rel="apple-touch-icon image_src" href="./apple-touch-icon.png">
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,900,900i|Montserrat:400,700,800|Noto+Sans:700|Open+Sans:300,400,700,800,800i|Poppins:900" rel="stylesheet">
-        
-        <link rel="stylesheet" href="/css/main.css">
-    </head>
-
-    <body>
-        <div class="wrapper">
-            <header id="header--main">
-                <div id="box--logomarca">
-                    <img src="imgs/structor/logomarca.png" alt="Farina Sistema de Ensino" class="logo--header">
-                </div>
-            </header>
-
-            <section id="sec--home">
-                <div class="banner--home"></div>
-            </section>
+    <div class="banner--home crop width--calc">
+        <div class="wrap-video">
+            <!-- <img src="/imgs/structor/Banner.png" alt="" srcset=""> -->
+            <video poster="{{ asset('/imgs/structor/Banner.png') }}" autoplay loop muted tabindex="0">
+                <source src="{{ asset('/videos/farina-aprova.mp4') }}" type="video/mp4">
+                Seu navegador não suporta o elemento <code>video</code>.
+            </video>
         </div>
+    </div>
 
+{{-- @if (Route::has('login'))
+    <div class="top-right links">
+        @auth
+            <a href="{{ url('/home') }}">Home</a>
+        @else
+            <a href="{{ route('login') }}">Login</a>
+            <a href="{{ route('register') }}">Register</a>
+        @endauth
+    </div>
+@endif --}}
 
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@endsection

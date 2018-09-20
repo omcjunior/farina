ef5f8b651363e88b4253e258b06e1683f1233ecc
Osvaldo Moreira <osvaldomoreira@gmail.com>
2018-09-19 21:24:18 -0400

    Retirar meta tag csrf-token

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
 
     <meta name="keywords" content="ensino, aprovação, aprovados, aprovações, aprova, vestibular, medicina, direito, engenharia, escola">
     <meta name="description" content="Desenvolver e proliferar saberes com o objetivo de contribuir com a formação de pessoas.">
